2453f2ae82db9ceb225ab37ce0aae406f0f66d65
Henrique <henrique.lopesdesouza@gmail.com>
2023-01-23 23:00:12 -0300

    async delete

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,6 +4,8 @@ import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 import { useThunk } from "../hooks/use-hooks";
+import UserListItem from "./UsersListItem";
+
 function UserList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
     useThunk(fetchUsers);
@@ -20,34 +22,27 @@ function UserList() {
     doCreateUser();
   };
 
+  let content;
   if (isLoadingUsers) {
-    return <Skeleton times={6} className="h-10 w-full" />;
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error Fetching data...</div>;
+  } else {
+    content = data.map((user) => {
+      return <UserListItem key={user.id} user={user} />;
+    });
   }
-  if (loadingUsersError) {
-    return <div>Error Fetching data...</div>;
-  }
-
-  const rederedUsers = data.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className=" flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
-        </div>
-      </div>
-    );
-  });
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
 
         <Button loading={isCreatingUser} onClick={handleUserAdd}>
-          {" "}
           + add User
         </Button>
       </div>
-      {rederedUsers}
+      {content}
     </div>
   );
 }
--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { addUser } from "../thunks/addUser";
 import { fetchUsers } from "../thunks/fetchUsers";
-
+import { removeUser } from "../thunks/removeUser";
 const userSlice = createSlice({
   name: "users",
   initialState: {
@@ -30,6 +30,21 @@ const userSlice = createSlice({
       state.data.push(action.payload);
     });
     builder.addCase(addUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+
+    builder.addCase(removeUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(removeUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      // bug console.log(action);
+      state.data = state.data.filter((user) => {
+        return user.id !== action.payload.id;
+      });
+    });
+    builder.addCase(removeUser.rejected, (state, action) => {
       state.isLoading = false;
       state.error = action.error;
     });
