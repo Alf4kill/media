a85a148a035a77addbd14e55ec474f02f2bfd936
Henrique <henrique.lopesdesouza@gmail.com>
2023-01-29 23:10:13 -0300

    final commit

---
+++ src/components/AlbumListItem.js
@@ -0,0 +1,30 @@
+import Button from "./Button";
+import ExpandanblePanel from "./ExpandablePanel";
+import { GoTrashcan } from "react-icons/go";
+import { useRemoveAlbumMutation } from "../store";
+import PhotosList from "./PhotosList";
+
+function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+  const header = (
+    <>
+      <Button
+        className="mr-2"
+        loading={results.isLoading}
+        onClick={handleRemoveAlbum}
+      >
+        <GoTrashcan />
+      </Button>
+      {album.title}
+    </>
+  );
+  return (
+    <ExpandanblePanel key={album.id} header={header}>
+      <PhotosList album={album} />
+    </ExpandanblePanel>
+  );
+}
+export default AlbumsListItem;
